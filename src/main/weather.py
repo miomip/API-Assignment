c83c9cad82d3327d19bbf95acf9ff1bb8fe1c63f
miomip <88206755+miomip@users.noreply.github.com>
2025-12-01 09:57:26 +0100

    Fixed longitude and latitude.

--- src/main/weather.py
+++ src/main/weather.py
@@ -1,8 +1,8 @@
 import requests
 
 def getWeatherInfo() -> dict | Exception:
-    lat: int = 10
-    lon: int = -60
+    lat: int = 60
+    lon: int = 10
     key: str = "7be1f4f13ae9483c08b4762c1f473765"
     req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}")
     if req.status_code == 200:
