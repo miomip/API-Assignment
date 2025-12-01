8cf979be4c508ea8de3080e7724debf802ccd4d8
miomip <88206755+miomip@users.noreply.github.com>
2025-12-01 09:39:23 +0100

    Made getCatFact to an function.

--- src/main/catfact.py
+++ src/main/catfact.py
@@ -1,8 +1,10 @@
 import requests
 
-req = requests.get("https://catfact.ninja/fact")
 
-if req.status_code == 200:
-    print(req.json()["fact"])
-else:
-    print(f"Error: {req.status_code}")
+def getCatFact() -> str | Exception:
+    req = requests.get("https://catfact.ninja/fact")
+
+    if req.status_code == 200:
+        return req.json()["fact"]
+    else:
+        return Exception(f"Could not get catfact. Error: {req.status_code}")
